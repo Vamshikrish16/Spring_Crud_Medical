67185de76a65f20b8aa2ec7f82967e9f78944572
Vamshikrish16 <107782096+Vamshikrish16@users.noreply.github.com>
2022-06-19 10:07:56 -0400

    Update Config.java

--- src/main/java/com/example/crud/entity/Config.java
+++ src/main/java/com/example/crud/entity/Config.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
-        private final String p = "Kranthi";
+        private final String p = "vamshii";
 
         @Bean(name="p")
         public String getP(){return p;}
